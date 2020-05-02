d4c0327826e689dfb9d4980f35971406dbc755c8
berkgaffaroglu <62242845+berkgaffaroglu@users.noreply.github.com>
2020-05-02 21:47:05 +0300

    Create app.js

---
+++ templates/app.js
@@ -0,0 +1,23 @@
+function changeWidth() {
+	
+	completed = document.getElementById('completedtasks').value;
+	alltasks =  document.getElementById('amountoftasks').value;
+	
+	completed = parseFloat(completed);
+	alltasks = parseFloat(alltasks);
+	
+
+	var result = (100*completed)/alltasks;
+	
+	if(isFinite(result) && completed <= alltasks) {
+		
+	    document.getElementById('completedprogress').style.width = result + "%";
+	    document.getElementById('completedprogress').innerHTML = "%" + result;
+		
+	}
+
+	
+	
+	
+	
+}
